00a178b43f6a2d6d1c57c26913e4f621645742c1
t_nakata <git.nakata0615@gmial.com>
2018-10-20 11:38:26 +0900

    RecyclerViewへ変更 1問追加

--- app/src/main/java/com/example/nakata/t/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/nakata/t/kotlinstudy/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.nakata.t.kotlinstudy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_log.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +19,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        log.apply {
-            movementMethod = ScrollingMovementMethod.getInstance()
+        log_recycler.apply {
+            adapter = LogAdapter()
         }
 
         run_button.setOnClickListener{
             when (spinner.selectedItem) {
                 "０から１００までの数字を出力" -> logToZeroToHundred()
                 "FizzBuzz" -> logToFizzBuzz()
+                "1から10のランダムな値を出力" -> logToRandom()
                 "リセット" -> resetLog()
             }
         }
@@ -43,17 +49,24 @@ class MainActivity : AppCompatActivity() {
         refreshLog()
     }
 
+    private fun logToRandom() {
+        // 1から10のランダムな値を出力
+
+        resetLog()
+    }
+
 
     private fun getMethodList(): List<String> {
         return listOf(
             "０から１００までの数字を出力",
             "FizzBuzz",
+            "1から10のランダムな値を出力",
             "リセット"
         )
     }
 
     private fun addLog(log: String) {
-        logs.add(log)
+        logs.add(String.format("%05d: %s", logs.size, log))
         refreshLog()
     }
 
@@ -63,23 +76,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun refreshLog() {
-        var logText = ""
-        for ((index, text) in logs.withIndex()) {
-            logText += String.format("%05d: %s\n", index, text)
+        log_recycler.scrollToPosition(logs.size - 1)
+        log_recycler.adapter?.notifyDataSetChanged()
+    }
+
+    inner class LogAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            return ViewHolder(LayoutInflater.from(this@MainActivity)
+                    .inflate(R.layout.item_log, parent, false))
         }
 
-        log.apply {
-            scrollY = log.height
-            text = logText
+        override fun getItemCount(): Int = logs.size
 
-            val scrollAmount = layout.getLineTop(lineCount) - height
-
-            if (scrollAmount > 0) {
-                scrollTo(0, scrollAmount)
-            } else {
-                scrollTo(0,0)
+        override fun onBindViewHolder(vh: RecyclerView.ViewHolder, position: Int) {
+            if (vh is ViewHolder) {
+                val item = logs[position]
+                item.apply {
+                    vh.log.text = item
+                }
             }
         }
+
+        inner class ViewHolder(view: View): RecyclerView.ViewHolder(view)  {
+            val log: TextView = view.text
+        }
+
     }
 
 }
